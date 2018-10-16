e062b9afca09477a29e75b81555ccb7f19ba1a99
yaochenfeng <282696845@qq.com>
2018-10-16 23:01:59 +0800

    update urls

--- loveshare/urls.py
+++ loveshare/urls.py
@@ -6,8 +6,15 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html")),
 ]
+
+if settings.APP_ENV == 'docker':
+    urlpatterns = urlpatterns \
+                  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+                  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
