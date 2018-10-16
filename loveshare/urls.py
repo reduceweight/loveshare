d9c6be9fd93568788c289675a4d381ec3931045a
yaochenfeng <282696845@qq.com>
2018-10-16 22:44:58 +0800

    rebase django

--- loveshare/urls.py
+++ loveshare/urls.py
@@ -2,31 +2,12 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 from django.views.generic import TemplateView
-from loveshare.api import router
-import share.urls
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="index.html")),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/auth/', include('rest_framework.urls')),
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
-if settings.APP_ENV == 'docker':
-    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
