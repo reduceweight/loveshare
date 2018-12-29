f6a7ab28ae61a1103b256a2d1fbc0faedad1a84c
yaochenfeng <282696845@qq.com>
2018-12-29 21:40:19 +0800

    update urls

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
-
+from loveshare.router import router
 from . import viewset
 # Routers provide an easy way of automatically determining the URL conf.
-viewset.router.register(r'users', viewset.UserViewSet)
+router.register(r'users', viewset.UserViewSet)
 
 
 urlpatterns = [
