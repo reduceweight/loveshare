e4c0ee2fadd0f285d09f7f6da52c6a343bcc55fe
yaochenfeng <282696845@qq.com>
2019-04-09 10:45:15 +0800

    add error

---
+++ apps/core/views.py
@@ -0,0 +1,34 @@
+from django.http import Http404
+from rest_framework import exceptions, status
+from django.core.exceptions import PermissionDenied
+from rest_framework.response import Response
+
+def exception_handler(exc, context):
+    """
+    Returns the response that should be used for any given exception.
+
+    By default we handle the REST framework `APIException`, and also
+    Django's built-in `Http404` and `PermissionDenied` exceptions.
+
+    Any unhandled exceptions may return `None`, which will cause a 500 error
+    to be raised.
+    """
+    if isinstance(exc, Http404):
+        exc = exceptions.NotFound()
+    elif isinstance(exc, PermissionDenied):
+        exc = exceptions.PermissionDenied()
+
+    if isinstance(exc, exceptions.APIException):
+        headers = {}
+        if getattr(exc, 'auth_header', None):
+            headers['WWW-Authenticate'] = exc.auth_header
+        if getattr(exc, 'wait', None):
+            headers['Retry-After'] = '%d' % exc.wait
+
+        if isinstance(exc.detail, (list, dict)):
+            data = exc.detail
+        else:
+            data = {'detail': exc.detail}
+        return Response(data, status=exc.status_code, headers=headers)
+
+    return None
