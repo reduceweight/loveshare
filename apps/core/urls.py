f29664525411b34682d237472b6b1304254a867d
yaochenfeng <282696845@qq.com>
2019-04-09 10:12:18 +0800

    api 调整

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,10 +4,13 @@ from loveshare.router import router
 from . import viewset
 # Routers provide an easy way of automatically determining the URL conf.
 router.register(r'users', viewset.UserViewSet )
-router.register(r'public', viewset.ImageViewSet)
-
+# router.register(r'public', viewset.ImageViewSet)
+img_upload = viewset.ImageViewSet.as_view({'post':'upload_img'})
+file_upload = viewset.ImageViewSet.as_view({'post':'upload_file'})
 urlpatterns = [
     path('auth', include('rest_framework.urls')),
     path('token/login', obtain_jwt_token),
     path('token/verify', verify_jwt_token),
+    path('public/imgupload', img_upload),
+    path('public/fileupload', file_upload),
 ]
